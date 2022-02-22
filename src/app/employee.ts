4e6a4c6c70926a9d1050f8e08ea34193f3a10ddf
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-02-22 21:33:06 +0800

    suspicious_unicorn_frontEnd

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee{
+  id: number;
+  email: string;
+  employeeCode: string;
+  imageUrl: string;
+  jobTitle: string;
+  location: string;
+  name: string;
+  phone: string;
+}
